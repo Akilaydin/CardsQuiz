ce1a9a30374d0711f82871e47cd97fdd31e2df32
Artem Ovchinnikov <artem.o@amayasoft.uz>
2022-01-28 11:51:04 +0300

    Scripts changed

--- Assets/Scripts/GameLifetimeScope.cs
+++ Assets/Scripts/GameLifetimeScope.cs
@@ -1,13 +1,20 @@
-﻿using VContainer;
+﻿using UnityEngine;
+
+using VContainer;
 using VContainer.Unity;
 
 public class GameLifetimeScope : LifetimeScope
 {
+	[SerializeField]
+	private HelloScreen _helloScreen;
+	
 	protected override void Configure(IContainerBuilder builder)
 	{
 		builder.RegisterEntryPoint<GamePresenter>();
 
 		builder.Register<HelloWorldService>(Lifetime.Singleton);
+
+		builder.RegisterComponent(_helloScreen);
 	}
 }
 
